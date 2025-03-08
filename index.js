5ab3f3f1126ba0a1e79be29a77444a09da3ad7f7
Quang Thi <dinhquangthi195@gmail.com>
2025-03-08 11:59:12 +0700

    add header

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", (event) => {
+  new Menu();
   const swiper = new Swiper(".swiper", {
     loop: true,
     effect: "fade",
@@ -7,3 +8,30 @@ document.addEventListener("DOMContentLoaded", (event) => {
     },
   });
 });
+
+class Menu {
+  constructor() {
+    this.trigger = document.querySelector(".hambuger");
+    this.wrap = document.querySelector(".menu");
+
+    this.trigger.addEventListener("click", this.handle.bind(this));
+  }
+  handle() {
+    if (!this.trigger.classList.contains("active")) {
+      this.open();
+    } else {
+      this.close();
+    }
+  }
+
+  open() {
+    this.trigger.classList.add("active");
+    this.wrap.classList.add("active");
+    document.body.style.overflow = "hidden";
+  }
+  close() {
+    this.trigger.classList.remove("active");
+    this.wrap.classList.remove("active");
+    document.body.style.overflow = "";
+  }
+}
